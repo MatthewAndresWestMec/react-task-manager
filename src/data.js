b653d452bc3c71202ff0b39eaa746d13e549f2fb
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2024-01-24 15:09:02 -0700

    finished functionality

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const tasksData = {
+    tasks: [
+      {
+        id: 1,
+        name: 'Push Day',
+        description: 'Focus on chest, shoulders, and triceps.',
+        category: 'Gym',
+      },
+      {
+        id: 2,
+        name: 'Pull Day',
+        description: 'Target back and biceps during this workout.',
+        category: 'Gym',
+      },
+      {
+        id: 3,
+        name: 'Leg Day',
+        description: 'Work on your lower body, including quads, hamstrings, and calves.',
+        category: 'Gym',
+      },
+      {
+        id: 4,
+        name: 'Active Rest Day',
+        description: 'Engage in light activities like walking, stretching, or yoga.',
+        category: 'Rest Day',
+      },
+      {
+        id: 5,
+        name: 'Full Rest Day',
+        description: 'Take a complete day off from any physical activity.',
+        category: 'Rest Day',
+      },
+    ],
+    categories: [
+      {
+        id: 1,
+        name: 'Gym',
+      },
+      {
+        id: 2,
+        name: 'Rest Day',
+      },
+    ],
+  };
+  
+  export default tasksData;
+  
